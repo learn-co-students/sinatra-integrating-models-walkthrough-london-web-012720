b7595816948f8e343ae9427a4e59aba7d2b51e94
Sergio Robles <sroblesc@gmail.com>
2020-02-18 10:12:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,12 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    #This is the long version:
+    #text_from_user = params[:user_text]
+    #@analyzed_text = TextAnalyzer.new(text_from_user)
+
+    #And this is what we should have:
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
